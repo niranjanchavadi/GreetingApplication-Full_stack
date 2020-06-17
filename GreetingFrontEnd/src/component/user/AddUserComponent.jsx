6e39466db4a8fb8d1b42d07984be7498853a4b13
Your Name <nnchavadi2@gmail.com>
2020-06-17 17:49:36 +0530

    [niranjanchavadi]ADDED  to added firt and last name with given page

---
+++ GreetingFrontEnd/src/component/user/AddUserComponent.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import ApiService from "../../service/ApiService";
+
+class AddUserComponent extends Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            firstName: '',
+            lastName: '',
+            message: null
+        }
+        this.saveUser = this.saveUser.bind(this);
+    }
+
+    saveUser = (e) => {
+        e.preventDefault();
+        let user = { firstName: this.state.firstName, lastName: this.state.lastName};
+        ApiService.addUser(user)
+            .then(res => {
+                this.setState({message : 'User added successfully.'});
+                this.props.history.push('/users');
+            });
+    }
+
+    onChange = (e) =>
+        this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        return(
+            <div>
+                <h2 className="text-center">Add Greeting</h2>
+                <form>
+
+                <div className="form-group">
+                    <label>First Name:</label>
+                    <input placeholder="First Name" name="firstName" className="form-control" value={this.state.firstName} onChange={this.onChange}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Last Name:</label>
+                    <input placeholder="Last name" name="lastName" className="form-control" value={this.state.lastName} onChange={this.onChange}/>
+                </div>
+                <button className="btn btn-success" onClick={this.saveUser}>Save</button>
+            </form>
+    </div>
+        );
+    }
+}
+
+export default AddUserComponent;
